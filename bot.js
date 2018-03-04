760752a1fe9b8d9a25f2991c6b602d2844c815d2
SunburntRock89 <SunburntRock89@gmail.com>
2018-03-04 12:55:58 +0000

    test

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on("message", async msg => {
 	if (!msg.content.startsWith(config.prefix)) return;
 	let cmd = msg.content.split(" ")[0].trim().toLowerCase().replace(config.prefix, "");
 	const suffix = msg.content.split(" ").splice(1).join(" ")
-		.trim();
+	.trim();
 
 	let cmdFile;
 	try {
@@ -41,9 +41,10 @@ client.on("voiceStateUpdate", async(oldMember, newMember) => {
 	if (!newMember.voiceChannel || newMember.voiceChannelID !== config.private) return;
 	let call = currentCall.find(r => r.status === true);
 	if (!call) return;
-	if (call.owner === oldMember.id || call.participants.includes(oldMember.id)) return;
-	if (oldMember.voiceChannelID) return newMember.setVoiceChannel(oldMember.voiceChannel);
-	newMember.setVoiceChannel(config.squaddy);
+	if (call.owner !== oldMember.user.id || !call.participants.includes(oldMember.user.id)) {
+		if (oldMember.voiceChannelID) return newMember.setVoiceChannel(oldMember.voiceChannel);
+		newMember.setVoiceChannel(config.squaddy);
+	}
 });
 
 client.memberSearch = async(string, server, toReturn) => new Promise((resolve, reject) => {
@@ -61,7 +62,7 @@ client.memberSearch = async(string, server, toReturn) => new Promise((resolve, r
 	}
 	if (string.lastIndexOf("#") === string.length - 5 && !isNaN(string.substring(string.lastIndexOf("#") + 1))) {
 		foundMember = server.members.filter(member => member.user.username === string.substring(0, string.lastIndexOf("#") + 1))
-			.find(member => member.user.discriminator === string.substring(string.lastIndexOf("#") + 1));
+		.find(member => member.user.discriminator === string.substring(string.lastIndexOf("#") + 1));
 	}
 	if (!foundMember) {
 		foundMember = server.members.find(member => member.user.username.toLowerCase() === string.toLowerCase());
@@ -76,5 +77,14 @@ client.memberSearch = async(string, server, toReturn) => new Promise((resolve, r
 		reject(new Error("Failed to find member"));
 	}
 });
+
+setInterval(async() => {
+	let call = currentCall.find(s => s.status === true);
+	if (!call) return;
+	let private = client.channels.get(config.private);
+	if (private.members.size >= 1) return;
+	currentCall.splice(currentCall.indexOf(call), 1);
+	client.channels.get("417089769323888641").send("Current private call has been ended as there was nobody in the channel.")
+}, 300000);
 
 client.login(auth.token);
